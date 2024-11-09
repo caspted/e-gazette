72f74beb89421a168c929cd018e728cf12794097
caspted <teddiejohn.rajeev26@gmail.com>
2024-11-09 15:48:16 +0800

    added navigation to admin-events page

---
+++ app/admin/events/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/admin/Layout";
+
+export default function AdminEvents() {
+  return (
+    <Layout>
+      <div>
+        <h1>Events is here</h1>
+      </div>
+    </Layout>
+  )
+}
+
